a041345752ba194bce77b0f7795d64926d42173c
PagotDFlow <webflopgt@gmail.com>
2023-01-24 21:51:47 +0100

    jwt-token initializing

---
+++ friendzone-back/services/jwt.service.ts
@@ -0,0 +1,27 @@
+﻿import * as jwt from 'jsonwebtoken';
+export class JwtService {
+    public secret: string;
+    
+    constructor() {
+        this.secret = process.env.TOKEN_KEY || 'secret';
+    }
+    
+    sign(payload: any): string {
+        return jwt.sign(payload, this.secret);
+    }
+    
+    verify(req, res, next): void {
+        const token = req.headers['x-access-token'];
+        const secret = process.env.TOKEN_KEY || 'secret';
+        if (!token) {
+            return res.status(403).send({auth: false, message: 'No token provided.'});
+        }
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) {
+                return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+            }
+            req.userId = decoded.id;
+            next();
+        });
+    }
+}
