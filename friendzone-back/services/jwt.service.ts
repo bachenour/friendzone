48efb941c159f2915d16e5b26b68be1bea23f708
PagotDFlow <webflopgt@gmail.com>
2023-03-22 12:34:35 +0100

    initialisation clé publiques et privées

--- friendzone-back/services/jwt.service.ts
+++ friendzone-back/services/jwt.service.ts
@@ -1,4 +1,10 @@
 ﻿import * as jwt from 'jsonwebtoken';
+import * as Fs from "fs";
+
+interface Keys {
+    private: string;
+    public: string;
+}
 export class JwtService {
     public secret: string;
     
@@ -7,21 +13,29 @@ export class JwtService {
     }
     
     sign(payload: any): string {
-        return jwt.sign(payload, this.secret);
+        const keys = this.getPublicAndPrivateKeys();
+        return jwt.sign(payload, {key: keys.private, passphrase: process.env.PRIVATE_KEY_PASSPHRASE}, {expiresIn: '3h'});
     }
     
     verify(req, res, next): void {
         const token = req.headers['x-access-token'];
-        const secret = process.env.TOKEN_KEY || 'secret';
+        const keys = this.getPublicAndPrivateKeys();
         if (!token) {
             return res.status(403).send({auth: false, message: 'No token provided.'});
         }
-        jwt.verify(token, secret, (err, decoded) => {
+        jwt.verify(token, keys.public , (err, decoded) => {
             if (err) {
                 return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
             }
             req.userId = decoded.id;
             next();
         });
+    }
+    
+   
+    getPublicAndPrivateKeys(): Keys {
+        const privateKey = Fs.readFileSync('./configs/back.private-key.pem', 'utf8');
+        const publicKey = Fs.readFileSync('./configs/back.public-key.pem', 'utf8');
+        return { private : privateKey, public : publicKey};
     }
 }
