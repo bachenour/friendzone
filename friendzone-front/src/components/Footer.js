0e01e64270238c5a31ecb66ca7e5b8736b32bbaa
PagotDFlow <webflopgt@gmail.com>
2023-05-14 21:28:06 +0200

    react routing

---
+++ friendzone-front/src/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+function footer(){
+    return(
+        <footer className="footer mt-auto py-3 bg-transparent fixed-bottom">
+            <div className="container text-center ">
+                <span className="text-white">2023© FrienDZone</span>
+            </div>
+        </footer>
+    )
+}
+
+export default footer;
