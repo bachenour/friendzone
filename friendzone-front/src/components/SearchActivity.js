dcc876d05fdb33cccb839699edeb404b9eb192b5
ABDELHAKIM NEKKACHE <abdelhakim.nekkache@atos.net>
2023-05-22 00:19:20 +0200

    mise en place et test d'un select pour le search activity / debug

--- friendzone-front/src/components/SearchActivity.js
+++ friendzone-front/src/components/SearchActivity.js
@@ -3,8 +3,15 @@ import React from "react";
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { purple } from '@mui/material/colors';
+import Select from 'react-select';
 
 const listActivity = [
+    // {label: 'Bowling', value: 'Bowling'},
+    // {label: 'Billard', value: 'Billard'},
+    // {label: 'Restaurant', value: 'Restaurant'},
+    // {label: 'shopping', value: 'shopping'},
+    // {label: 'Cinéma', value: 'Cinéma'},
+    // {label: 'Laser-game', value: 'Laser-game'},
     "Bowling",
     "Billard",
     "Restaurant",
@@ -25,11 +32,13 @@ function SearchActivity() {
 
     React.useEffect(() => {
         let results = []
+        console.log("searchTerm : " + searchTerm)
         if (searchTerm !== '') {
             results = listActivity.filter(activity =>
                 activity.toLowerCase().startsWith(searchTerm.toLowerCase().slice(0, 3))
             );
             setSearchResults(results);
+            console.log("searchResults : " + searchResults)
         } else if (searchTerm === '') {
             setSearchResults([]);
         }
@@ -60,10 +69,12 @@ function SearchActivity() {
                 <label for="searchBarCity" className='searchBar'>OU ?</label>
                 <input type="text" name="searchBar" id="searchBarCity" placeholder="Ville, code postal..." />
                 <label for="searchBarActivity" className='searchBar'>QUOI ?</label>
-                <input type="text" name="searchBar" id="searchBarActivity" placeholder="Rechercher une activité" value={searchTerm} onChange={handleChange} />
+                {/* <input type="text" name="searchBar" id="searchBarActivity" placeholder="Rechercher une activité" value={searchTerm} onChange={handleChange} /> */}
+                <Select value={searchTerm} onChange={handleChange} options={searchResults}/>
             </div>
             <div className="search__results">
-            <ColorButton variant="contained">Rechercher</ColorButton>                <ul>
+            <ColorButton variant="contained">Rechercher</ColorButton>                
+                <ul>
                     <li>
                         <li>{searchResults}</li>
                     </li>
