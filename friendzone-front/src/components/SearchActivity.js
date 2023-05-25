b00a87b6680139830b5def6e043c9b96eb1584c8
ABDELHAKIM NEKKACHE <abdelhakim.nekkache@atos.net>
2023-05-26 01:13:21 +0200

    redesign du searchActivity plus modification de la logique de recherche d'activité

--- friendzone-front/src/components/SearchActivity.js
+++ friendzone-front/src/components/SearchActivity.js
@@ -1,87 +1,97 @@
-import '../styles/SearchActivity.css'
+import "../styles/SearchActivity.css";
 import React from "react";
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import { purple } from '@mui/material/colors';
-import Select from 'react-select';
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { purple } from "@mui/material/colors";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+
 
 const listActivity = [
-    // {label: 'Bowling', value: 'Bowling'},
-    // {label: 'Billard', value: 'Billard'},
-    // {label: 'Restaurant', value: 'Restaurant'},
-    // {label: 'shopping', value: 'shopping'},
-    // {label: 'Cinéma', value: 'Cinéma'},
-    // {label: 'Laser-game', value: 'Laser-game'},
-    "Bowling",
-    "Billard",
-    "Restaurant",
-    "shopping",
-    "Cinéma",
-    "Laser-game"
-]
+  // {label: 'Bowling', value: 'Bowling'},
+  // {label: 'Billard', value: 'Billard'},
+  // {label: 'Restaurant', value: 'Restaurant'},
+  // {label: 'shopping', value: 'shopping'},
+  // {label: 'Cinéma', value: 'Cinéma'},
+  // {label: 'Laser-game', value: 'Laser-game'},
+  "Bowling",
+  "Billard",
+  "Restaurant",
+  "shopping",
+  "Cinéma",
+  "Laser-game",
+];
 
-let cityResults = []
+let cityResults = [];
 
 function SearchActivity() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    const [searchTerm, setSearchTerm] = React.useState("");
-    const [searchResults, setSearchResults] = React.useState([]);
-    const handleChange = event => {
-        setSearchTerm(event.target.value);
-    };
+  React.useEffect(() => {
+    let results = [];
+    console.log("searchTerm : " + searchTerm);
+    if (searchTerm !== "") {
+      results = listActivity.filter((activity) =>
+        activity.toLowerCase().startsWith(searchTerm.toLowerCase().slice(0, 3))
+      );
+      setSearchResults(results);
+      console.log("searchResults : " + searchResults);
+    } else if (searchTerm === "") {
+      setSearchResults([]);
+    }
+  }, [searchTerm]);
 
-    React.useEffect(() => {
-        let results = []
-        console.log("searchTerm : " + searchTerm)
-        if (searchTerm !== '') {
-            results = listActivity.filter(activity =>
-                activity.toLowerCase().startsWith(searchTerm.toLowerCase().slice(0, 3))
-            );
-            setSearchResults(results);
-            console.log("searchResults : " + searchResults)
-        } else if (searchTerm === '') {
-            setSearchResults([]);
-        }
-    }, [searchTerm]);
+  const handleResultClick = (result) => {
+    setSearchTerm(result);
+  };
 
-    fetch("https://geo.api.gouv.fr/communes?nom="+ searchTerm +"&fields=departement&boost=population&limit=5", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "GET"
-      })
+  fetch(
+    "https://geo.api.gouv.fr/communes?nom=" +
+      searchTerm +
+      "&fields=departement&boost=population&limit=5",
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => (cityResults = json));
 
-        .then(response => response.json())
-        .then(json => cityResults = json);
-
-        const ColorButton = styled(Button)(({ theme }) => ({
-            color: theme.palette.getContrastText(purple[500]),
-            backgroundColor: purple[500],
-            '&:hover': {
-              backgroundColor: purple[700],
-            },
-          }));
-
-    return (
-        <>
-            <div className="searchActivity">
-                <label for="searchBarCity" className='searchBar'>OU ?</label>
-                <input type="text" name="searchBar" id="searchBarCity" placeholder="Ville, code postal..." />
-                <label for="searchBarActivity" className='searchBar'>QUOI ?</label>
-                {/* <input type="text" name="searchBar" id="searchBarActivity" placeholder="Rechercher une activité" value={searchTerm} onChange={handleChange} /> */}
-                <Select value={searchTerm} onChange={handleChange} options={searchResults}/>
-            </div>
-            <div className="search__results">
-            <ColorButton variant="contained">Rechercher</ColorButton>                
+  return (
+    <>
+      <div className="bar">
+        <div className="check-out">
+          <p>OU ?</p>
+          <input type="text" placeholder="Ville, code postal..."></input>
+        </div>
+        <div className="guests">
+          <p>QUOI ?</p>
+          <input type="text" value={searchTerm} onChange={handleChange} placeholder="Rechercher une activité"></input>
+            <button>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+        </div>
+      </div>
+      <div className="search__results">
+        {searchResults.length > 0 && searchTerm !== '' && (
                 <ul>
-                    <li>
-                        <li>{searchResults}</li>
-                    </li>
+                    {searchResults.map((suggestion, index) =>(
+                        <li key={index} onClick={() => handleResultClick(suggestion)}>
+                            {suggestion}
+                        </li>
+                    ))}
                 </ul>
-            </div>
-        </>
-    )
+            )}             
+        </div>
+    </>
+  );
 }
 
-export default SearchActivity
+export default SearchActivity;
