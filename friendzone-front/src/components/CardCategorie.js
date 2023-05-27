b204d25e19d6d19c1cebe563370961145daf2a8b
ABDELHAKIM NEKKACHE <abdelhakim.nekkache@atos.net>
2023-05-27 12:30:52 +0200

    Modification de l'aspect du caroucelle et des card debug du css des card activity

--- friendzone-front/src/components/CardCategorie.js
+++ friendzone-front/src/components/CardCategorie.js
@@ -10,46 +10,46 @@ const listCategorieCard = [
         categorie: 'Jeux de tir', url: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/fc/f9/a3.jpg', description: "Jeux de tir comme le paintball ou le laser game"
     },
     {
-        categorie: 'Soirée dansante', url: 'https://www.lescoulissesdiscotheque.fr/public/img/big/AdobeStock278700158jpg_5e96d087451c7.jpg'
+        categorie: 'Soirée dansante', url: 'https://www.lescoulissesdiscotheque.fr/public/img/big/AdobeStock278700158jpg_5e96d087451c7.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Sortie culturelle', url: 'https://reyssouze.centres-sociaux.fr/files/2017/11/sorties-culturelles3.png'
+        categorie: 'Sortie culturelle', url: 'https://reyssouze.centres-sociaux.fr/files/2017/11/sorties-culturelles3.png', description: "Lorem ipsum"
     },
     {
-        categorie: 'Concert', url: 'https://img.freepik.com/photos-gratuite/public-enthousiaste-regardant-feux-artifice-confettis-s-amusant-lors-festival-musique-nuit-espace-copie_637285-559.jpg'
+        categorie: 'Concert', url: 'https://img.freepik.com/photos-gratuite/public-enthousiaste-regardant-feux-artifice-confettis-s-amusant-lors-festival-musique-nuit-espace-copie_637285-559.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Sport', url: 'https://img.freepik.com/vecteurs-libre/concept-equipement-sport_1284-13034.jpg?w=360'
+        categorie: 'Sport', url: 'https://img.freepik.com/vecteurs-libre/concept-equipement-sport_1284-13034.jpg?w=360', description: "Lorem ipsum"
     },
     {
-        categorie: 'Soirée jeux de société', url: 'http://www.mqvillejean.fr/images/369/soiree-jeu.jpg'
+        categorie: 'Soirée jeux de société', url: 'http://www.mqvillejean.fr/images/369/soiree-jeu.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Sortie chill', url: 'https://media.istockphoto.com/id/994808176/photo/happy-friends-cheering-and-drinking-cocktails-at-beach-party-outdoor-young-millennials-people.jpg?s=612x612&w=0&k=20&c=cx0_sX3s_t3kYQ5euzeUYYasmWA2u5Qi6cL3rCW9KHo='
+        categorie: 'Sortie chill', url: 'https://media.istockphoto.com/id/994808176/photo/happy-friends-cheering-and-drinking-cocktails-at-beach-party-outdoor-young-millennials-people.jpg?s=612x612&w=0&k=20&c=cx0_sX3s_t3kYQ5euzeUYYasmWA2u5Qi6cL3rCW9KHo=', description: "Lorem ipsum"
     },
     {
-        categorie: 'Activité en plein air', url: 'https://m1.quebecormedia.com/emp/emp/iStock_1096035138_copie347e0536-fe46-4d56-80c4-d0be45106279_ORIGINAL.jpg?impolicy=crop-resize&x=0&y=0&w=0&h=0&width=925'
+        categorie: 'Activité en plein air', url: 'https://m1.quebecormedia.com/emp/emp/iStock_1096035138_copie347e0536-fe46-4d56-80c4-d0be45106279_ORIGINAL.jpg?impolicy=crop-resize&x=0&y=0&w=0&h=0&width=925', description: "Lorem ipsum"
     },
     {
-        categorie: 'Cours de cuisine', url: 'https://www.wonderbox.fr/blog/wp-content/uploads/sites/4/2020/02/Nos-ateliers-culinaires-cours-de-cuisine-1-1.jpg'
+        categorie: 'Cours de cuisine', url: 'https://www.wonderbox.fr/blog/wp-content/uploads/sites/4/2020/02/Nos-ateliers-culinaires-cours-de-cuisine-1-1.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Bien-être', url: 'https://resize.elle.fr/original/var/plain_site/storage/images/minceur/bien-etre-relaxation/voici-la-tendance-bien-etre-de-2023-4077465/97831135-1-fre-FR/Voici-LA-tendance-bien-etre-de-2023.jpg'
+        categorie: 'Bien-être', url: 'https://resize.elle.fr/original/var/plain_site/storage/images/minceur/bien-etre-relaxation/voici-la-tendance-bien-etre-de-2023-4077465/97831135-1-fre-FR/Voici-LA-tendance-bien-etre-de-2023.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Parc & fun', url: 'https://ns328286.ip-37-187-113.eu/ew/wallpapers/800x480/14467_800x480.jpg'
+        categorie: 'Parc & fun', url: 'https://ns328286.ip-37-187-113.eu/ew/wallpapers/800x480/14467_800x480.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Ateliers', url: 'https://www.payssaintgilles-tourisme.fr/wp-content/uploads/wpetourisme/poterie-adulte-V.M.jpg'
+        categorie: 'Ateliers', url: 'https://www.payssaintgilles-tourisme.fr/wp-content/uploads/wpetourisme/poterie-adulte-V.M.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Activité aquatique', url: 'https://www.myluxurytravel.fr/wp-content/uploads/2017/07/towerofneptune-sharks-hotel-dubai-atlantis-palm.jpeg'
+        categorie: 'Activité aquatique', url: 'https://www.myluxurytravel.fr/wp-content/uploads/2017/07/towerofneptune-sharks-hotel-dubai-atlantis-palm.jpeg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Pilotage', url: 'https://www.motorsport-academy.fr/861-slider_produit/coffret-pilotage-elite.jpg'
+        categorie: 'Pilotage', url: 'https://www.motorsport-academy.fr/861-slider_produit/coffret-pilotage-elite.jpg', description: "Lorem ipsum"
     },
     {
-        categorie: 'Visites', url: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/7a/77/00/musee-des-beaux-arts.jpg?w=1200&h=1200&s=1'
+        categorie: 'Visites', url: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/7a/77/00/musee-des-beaux-arts.jpg?w=1200&h=1200&s=1', description: "Lorem ipsum"
     }
 ]
 
@@ -74,7 +74,7 @@ function CardCategorie() {
                                                 <span className="card__status">8 activité disponible</span>
                                             </div>
                                         </div>
-                                        <p className="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                                        <p className="card__description">{element.description}</p>
                                         <Button className='buttonCard' href='#' variant="contained">Rejoindre</Button>
                                     </div>
                                 </div>
