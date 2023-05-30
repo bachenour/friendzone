fa94735e477e26a1e427f18d198e86473c4d4004
PagotDFlow <webflopgt@gmail.com>
2023-05-30 21:28:23 +0200

    ajout MiddleWare

--- friendzone-back/routes/posts.route.ts
+++ friendzone-back/routes/posts.route.ts
@@ -2,9 +2,11 @@ import {Routes} from "../interfaces/routes.interface";
 import {Router} from 'express';
 import PostController from "../controllers/post.controller";
 import {PostDto} from "../dto/post.dto";
+import {JwtService} from "../services/jwt.service";
 
 const postControl = new PostController();
 const postsDto = new PostDto();
+const authMiddleware = new JwtService();
 
 export class PostsRoute implements Routes {
     public path = '/posts';
@@ -15,10 +17,10 @@ export class PostsRoute implements Routes {
     }
 
     private initializeRoutes() {
-        this.router.post(`${this.path}/addPost`, postsDto.addPost, postControl.addPost);
+        this.router.post(`${this.path}/addPost`, authMiddleware.verify, postsDto.addPost, postControl.addPost);
         this.router.get(`${this.path}/getPosts`, postControl.getPosts);
         this.router.get(`${this.path}/:id`, postsDto.findPostByUserId, postControl.getPostsByUserId);
-        this.router.put(`${this.path}/:id`, postControl.updatePost);
-        this.router.delete(`${this.path}/:id`, postControl.deletePostById);
+        this.router.put(`${this.path}/:id`, authMiddleware.verify,  postControl.updatePost);
+        this.router.delete(`${this.path}/:id`, authMiddleware.verify,  postControl.deletePostById);
     }
 }
