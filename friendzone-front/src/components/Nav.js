f30741c6bf88db996851e088432c3299aabf97aa
bachenour <bache.nour2@gmail.com>
2023-07-04 18:43:31 +0200

    logout + design pb footer under body register form

--- friendzone-front/src/components/Nav.js
+++ friendzone-front/src/components/Nav.js
@@ -4,8 +4,13 @@ import {Link} from "react-router-dom";
 import {faHouse} from "@fortawesome/free-solid-svg-icons/faHouse";
 import {faUser} from "@fortawesome/free-solid-svg-icons/faUser";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPowerOff} from "@fortawesome/free-solid-svg-icons";
+import {closeSession} from "./authentication/utils";
 
 function Nav() {
+    const logout = () => {
+        closeSession();
+    }; 
     return (
         <div className="backgroundNav">
             <nav className="navigation row align-items-center">
@@ -13,13 +18,20 @@ function Nav() {
                     <FontAwesomeIcon icon={faHouse} /> 
                     <Link to="/"> Accueil</Link>
                 </div>
-                <div className="col-2 text-center">
-                    <img className="logoFriendZone mx-auto d-block" src={logo}/>
-                </div>
-                {sessionStorage.getItem('firstName') && 
-                    <div className="col-2 text-end">
-                        <a><FontAwesomeIcon icon={faUser} />{sessionStorage.getItem('firstName')}</a>
-                    </div> 
+                {sessionStorage.getItem('firstName')&&
+                    <>
+                        <div className="col-2 text-center">
+                            <img className="logoFriendZone mx-auto d-block" src={logo}/>
+                        </div>    
+                        <div className="col-2 text-end">
+                            <span>
+                                <a><FontAwesomeIcon icon={faPowerOff} onClick={logout}/>  </a>
+                            </span>
+                            <span>
+                                <a><FontAwesomeIcon icon={faUser} /> {sessionStorage.getItem('firstName')}</a>
+                            </span>
+                        </div>
+                    </>
                 }
             </nav>
         </div>
