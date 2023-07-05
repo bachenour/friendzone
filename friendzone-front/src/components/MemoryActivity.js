554b93deb2727e941efe5fa24cb489d062f77ddb
PagotDFlow <webflopgt@gmail.com>
2023-07-06 00:38:23 +0200

    fetching activity

--- friendzone-front/src/components/MemoryActivity.js
+++ friendzone-front/src/components/MemoryActivity.js
@@ -1,59 +1,58 @@
 import '../styles/MemoryActivity.css'
 import cinemaMemory from '../assets/cinemaMemory.png'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
-
-const listMemoryCard = [
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-    {
-        categorie: 'Jeux de tir', src:{cinemaMemory}, auteur: "Lily"
-    },
-]
+import React, {useState} from "react";
+import axios from "axios";
+import {Box} from "@mui/material";
+import Post from "./Posts/Post";
+import uuid from "react-uuid";
+import {Link} from "react-router-dom";
 
 function MemoryActivity() {
+    const [posts, setPosts] = useState([]);
+
+    React.useEffect(() => {
+        let isMounted = true;
+        if (posts.length === 0) {
+            axios.get("http://127.0.0.1:3030/posts/getPosts", {
+                headers: {
+                    "Cache-Control": "no-cache"}
+            })
+                .then(response => {
+                    //limit to 4 posts
+                    response.data.posts = response.data.posts.slice(0, 2);
+                    setPosts(response.data.posts);
+                })
+        }
+        return () => { isMounted = false; }
+    }, []);
+    
     return (
         <>
-            <ul className='cardsMemory'>
-            {listMemoryCard.map((element, i) => {
+                {posts.map(({
+                                id,
+                                title,
+                                content,
+                                creationDate,
+                                users,
+                                opinion
+                            }) => {
                     return (
-                        <>
-                            <li key={i}>
-                                <div className='cardMemory'>
-                                        <img className='card_memory_image' src={cinemaMemory}></img>
-                                    <div className='cardTxt'>
-                                        <p>Hello World</p>
-                                    </div>
-                                </div>
-                            </li>
-                        </>
-                    )
-                })}
-            </ul>
+                        <Post
+                            key={uuid()}
+                            id={id}
+                            title={title}
+                            content={content}
+                            creationDate={creationDate}
+                            users={users}
+                            comments={opinion}
+                            isComments={false}
+                        />
+                    );
+                })};
+            );
             <div className='circleButton'>
-                <a href='#' className='iconCircleButton'><KeyboardArrowDownIcon></KeyboardArrowDownIcon></a>
+                <Link to={"/friendplace"} className='iconCircleButton'>Voir plus</Link>
             </div>
             <div className='memoryBanner'>
                 <p>avis</p>
